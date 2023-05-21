433229f95f22ec5586d909c849e837caddcb8c8b
Gitika Bansal <gitika23689@gmail.com>
2023-05-21 15:34:42 +0530

    Optimize logic

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -7,20 +7,18 @@ shift = int(input("Type the shift number:\n"))
 
 
 # Create a function called 'caesar' that takes the 'text' and 'shift' and 'direction' as inputs.
-def caesar(text, shift, direction):
+def caesar(input_text, shift_value, shift_direction):
     caesar_text = ""
-    for char in text:
+    if shift_direction == 'decode':
+        shift_value *= -1
+    for char in input_text:
         index = alphabet.index(char)
-        if direction == 'encode':
-            shifted_index = index + shift
-            if shifted_index > 25:
-                shifted_index = shifted_index - 26
-        else:
-            shifted_index = index - shift
-
+        shifted_index = index + shift_value
+        if shifted_index > 25:
+            shifted_index = shifted_index - 26
         caesar_text += alphabet[shifted_index]
 
-    if direction == 'encode':
+    if shift_direction == 'encode':
         print(f"The encoded text is {caesar_text}")
     else:
         print(f"The decoded text is {caesar_text}")
